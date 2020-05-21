ab9adde0268231a06674c27a3f3bd25d9875731f
Max-Andre <zilsmc@gmail.com>
2020-05-21 16:40:29 +0200

    completed phase 1

---
+++ main.js
@@ -0,0 +1,5 @@
+// main.js
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./main.scss";
